93db64eb02f6187655764bd335d1bb0d64cb0da6
Mac Ordan <marcordan@gmail.com>
2019-02-20 14:32:45 +0800

    add logout function

--- controllers/users.php
+++ controllers/users.php
@@ -12,4 +12,12 @@ class Users extends Controller {
         $this->returnView($viewmodel->login(), true);
     }   
 
+    protected function logout(){
+        unset($_SESSION['is_logged_in']);
+        unset($_SESSION['user_data']);
+        session_destroy();
+        //redirect
+        header('Location:'.ROOT_URL);
+    }
+
 }
