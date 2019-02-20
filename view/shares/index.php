b3fcece28be24bc0e88cc375736671f71af8173a
Mac Ordan <marcordan@gmail.com>
2019-02-20 14:40:42 +0800

    add access control

--- view/shares/index.php
+++ view/shares/index.php
@@ -1,5 +1,7 @@
 <div>
+    <?php if(isset($_SESSION['is_logged_in'])) : ?>
     <a href="<?php echo ROOT_URL; ?>shares/add" class="btn btn-success btn-share">Share Something</a>
+<?php endif;?>
     <?php foreach($viewModel as $item) : ?>
         <div class="card card-body bg-light">
             <h4><?php echo $item['title']; ?></h4>
